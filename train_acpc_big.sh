3d4a63f1f18a82e5163d862f63904149467ac9c4
Adrian Lancucki <16889482+alancucki@users.noreply.github.com>
2021-08-08 15:32:29 +0200

    Put _cache.txt in the exp dir

--- train_acpc_big.sh
+++ train_acpc_big.sh
@@ -1,5 +1,6 @@
-
+#
 # Based on the `CPC-big-kmeans50/cpc_ll6k` ZeroSpeech2021 baseline model config
+#
 
 : ${DATASET_DIR:="/datasets/libri-light/original/output_dir"}  # has small/ medium/ large/ and dev-clean from LS
 : ${OUTPUT_DIR:="results/acpc_big_ll60k_test"}
@@ -7,13 +8,8 @@
 
 mkdir -p $OUTPUT_DIR
 
-ARGS+=" --pathDB $DATASET_DIR"
-ARGS+=" --pathCheckpoint $OUTPUT_DIR"
 ARGS+=" --pathTrain filelists/ll-0.6k.txt"  # NOTE 0.6k only for debugging; use 6k or 60k
 ARGS+=" --pathVal filelists/ls-dev-clean.txt"
-ARGS+=" --file_extension .flac"
-ARGS+=" --n_process_loader 8"
-ARGS+=" --max_size_loaded 200000000"
 ARGS+=" --batchSizeGPU $BATCH_SIZE"
 # ARGS+=" --distributed"
 
@@ -36,5 +32,11 @@ ARGS+=" --schedulerRamp 10"
 # io
 ARGS+=" --save_step 1"
 ARGS+=" --loadCriterion"
+ARGS+=" --path_cache ${OUTPUT_DIR}/_cpc_cache.txt"
+ARGS+=" --pathDB $DATASET_DIR"
+ARGS+=" --pathCheckpoint ${OUTPUT_DIR}"
+ARGS+=" --file_extension .flac"
+ARGS+=" --n_process_loader 8"
+ARGS+=" --max_size_loaded 200000000"
 
 python -u cpc/train.py $ARGS 2>&1 | tee -ai ${OUTPUT_DIR}/acpc_big.log
