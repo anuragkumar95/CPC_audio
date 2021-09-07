b3351f2fe677bed7ead4138c3e08510b98398d00
tiagoCuervo <dscuervog@gmail.com>
2021-09-07 21:06:16 +0200

    Add script for lineval in Buckeye

---
+++ lineval_buckeye.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+set -x
+
+RVERB="-v --dry-run"
+RVERB=""
+CPC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SAVE_DIR="$(
+python - "$@" << END
+if 1:
+  import argparse
+  import os.path
+  parser = argparse.ArgumentParser(description='Process some integers.')
+  parser.add_argument('load', type=str,
+                        help="Path to the checkpoint to evaluate.")
+  parser.add_argument('--pathCheckpoint')
+  parser.add_argument('--CTC', action='store_true')
+  parser.add_argument('--get_encoded', action='store_true')
+  parser.add_argument('--CTC_forbid_blank', action='store_true')
+  parser.add_argument('--numLayers', type=int, default=2)
+  parser.add_argument('--CPCLevel', type=int, default=0, help="")
+  args, _ = parser.parse_known_args()
+  checkpoint_dir = os.path.dirname(args.load)
+  checkpoint_no = args.load.split('_')[-1][:-3]
+  eval_ctc = ""
+  if args.CTC:
+    eval_ctc += "_ctc"
+  if args.CTC_forbid_blank:
+    eval_ctc += "_noblank"
+  if args.get_encoded:
+    eval_ctc += "_onenc"
+  if args.numLayers:
+    eval_ctc += f"_{args.numLayers}layers"
+  if args.CPCLevel > 0:
+    eval_ctc += f"_level{args.CPCLevel}"
+  print(f"{checkpoint_dir}/linevalBuckeye{eval_ctc}_{checkpoint_no}")
+END
+)"
+
+mkdir -p ${SAVE_DIR}/code
+rsync --exclude '.*' \
+      --exclude data \
+      --exclude pretrained_models \
+      --exclude '__pycache__' \
+      --exclude '*runs*' \
+      --exclude '*.pyc' \
+      --exclude '*.ipynb' \
+      --filter=':- .gitignore' \
+    $RVERB -lrpt $CPC_DIR/ ${SAVE_DIR}/code/
+
+echo $0 "$@" >> ${SAVE_DIR}/out.txt
+exec python -u cpc/eval/linear_separability.py \
+    --pathDB /pio/scratch/1/i323106/data/BUCKEYE/train/ \
+    --load "$@" \
+    --pathPhone /pio/scratch/1/i323106/data/BUCKEYE/converted_aligned_phones.txt \
+    --file_extension .wav \
+    --pathCheckpoint $SAVE_DIR \
+    2>&1 | tee -ai ${SAVE_DIR}/out.txt
