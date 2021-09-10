Fix ground truth evals bug, add options for arch of phone classifier, add PER calculation to the lineval training process, and a bit of cleaning of the lineval script

cpc/criterion/criterion.py
@@ -6,7 +6,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from .custom_layers import EqualizedLinear, EqualizedConv1d
-from cpc.criterion.seq_alignment import collapseLabelChain
+from cpc.criterion.seq_alignment import collapseLabelChain, getSeqPER
+import random
 
 class Identity(nn.Module):
 
@@ -342,18 +343,26 @@ class SpeakerDoubleCriterion(BaseCriterion):
 
 class PhoneCriterion(BaseCriterion):
 
-    def __init__(self, dimEncoder, nPhones, onEncoder,
-                 nLayers=1):
+    def __init__(self, dimEncoder, nPhones, onEncoder, linear=False, useLSTM=True, 
+                 useConvClassifier=False):
 
         super(PhoneCriterion, self).__init__()
-        if nLayers == 1:
-            self.PhoneCriterionClassifier = nn.Linear(dimEncoder, nPhones)
+        self.useLSTM = useLSTM
+        self.useConvClassifier = useConvClassifier
+        d = 2 if useLSTM else 1
+        if linear:
+            self.PhoneCriterionClassifier = nn.Linear(dimEncoder * d, nPhones + 1)
+        elif useConvClassifier:
+            self.PhoneCriterionClassifier = torch.nn.Conv1d(
+                dimEncoder * d, nPhones + 1, 8, stride=4)
         else:
-            outLayers = [nn.Linear(dimEncoder, nPhones)]
-            for l in range(nLayers - 1):
-                outLayers.append(nn.ReLU())
-                outLayers.append(nn.Linear(nPhones, nPhones))
-            self.PhoneCriterionClassifier = nn.Sequential(*outLayers)
+            self.PhoneCriterionClassifier = nn.Sequential(
+                nn.Linear(dimEncoder * d, dimEncoder * 2*d),
+                nn.ReLU(),
+                nn.Linear(dimEncoder * 2*d, nPhones + 1),
+            )
+        if useLSTM:
+            self.lstm = torch.nn.LSTM(dimEncoder, dimEncoder, num_layers=1, batch_first=True, bidirectional=True)
 
         self.lossCriterion = nn.CrossEntropyLoss()
         self.onEncoder = onEncoder
@@ -371,31 +380,45 @@ class PhoneCriterion(BaseCriterion):
         acc = (predictions.max(1)[1] == label).double().mean().view(1, -1)
         return loss, acc
 
-    def getPrediction(self, cFeature):
-        batchSize, seqSize = cFeature.size(0), cFeature.size(1)
-        cFeature = cFeature.contiguous().view(batchSize * seqSize, -1)
-        output = self.PhoneCriterionClassifier(cFeature)
-        return output.view(batchSize, seqSize, -1)
+    def getPrediction(self, x):
+        if self.useLSTM:
+            try:
+                self.lstm.flatten_parameters()
+            except RuntimeError:
+                pass
+            x = self.lstm(x)[0]
+            if isinstance(x, torch.nn.utils.rnn.PackedSequence):
+                x = torch.nn.utils.rnn.pad_packed_sequence(x, batch_first=True)[0]
+        if self.useConvClassifier:
+            x = x.permute(0, 2, 1)
+            x = self.PhoneCriterionClassifier(x)
+            return x.permute(0, 2, 1)
+        else:
+            return self.PhoneCriterionClassifier(x)
 
 
 class CTCPhoneCriterion(BaseCriterion):
 
-    def __init__(self, dimEncoder, nPhones, onEncoder, nLayers=2, forbid_blank=False):
+    def __init__(self, dimEncoder, nPhones, onEncoder, linear=False, useLSTM=True, 
+                 useConvClassifier=False,forbid_blank=False):
 
         super(CTCPhoneCriterion, self).__init__()
-        self.linear = (nLayers == 1)
-        self.PhoneCriterionClassifier = nn.Linear(dimEncoder, nPhones + 1)
-        if not self.linear:
-            self.lstm = torch.nn.LSTM(dimEncoder, dimEncoder, num_layers=1, batch_first=True, 
-            bidirectional=True)
-            # self.PhoneCriterionClassifier = nn.Sequential(
-            #     nn.Linear(dimEncoder, 1024),
-            #     nn.ReLU(),
-            #     # nn.Dropout(0.5),
-            #     nn.Linear(1024, nPhones + 1),
-            # )
+        self.useLSTM = useLSTM
+        self.useConvClassifier = useConvClassifier
+        d = 2 if useLSTM else 1
+        if linear:
+            self.PhoneCriterionClassifier = nn.Linear(dimEncoder * d, nPhones + 1)
+        elif useConvClassifier:
             self.PhoneCriterionClassifier = torch.nn.Conv1d(
-                dimEncoder * 2, nPhones + 1, 8, stride=4)
+                dimEncoder * d, nPhones + 1, 8, stride=4)
+        else:
+            self.PhoneCriterionClassifier = nn.Sequential(
+                nn.Linear(dimEncoder * d, dimEncoder * 2*d),
+                nn.ReLU(),
+                nn.Linear(dimEncoder * 2*d, nPhones + 1),
+            )
+        if useLSTM:
+            self.lstm = torch.nn.LSTM(dimEncoder, dimEncoder, num_layers=1, batch_first=True, bidirectional=True)
         self.lossCriterion = nn.CTCLoss(blank=nPhones, zero_infinity=True)
         self.onEncoder = onEncoder
         self.BLANK_LABEL = nPhones
@@ -404,38 +427,41 @@ class CTCPhoneCriterion(BaseCriterion):
     def extra_repr(self):
         return f"CTCPhoneCriterion(..., onEncoder={self.onEncoder}, forbid_blank={self.forbid_blank})"
 
-    def getPrediction(self, cFeature):
-        B, S, H = cFeature.size()
-        if self.linear:
-            cFeature = cFeature.contiguous().view(B*S, H)
-            return self.PhoneCriterionClassifier(cFeature).view(B, S, -1)
-        else:
+    def getPrediction(self, x):
+        if self.useLSTM:
             try:
                 self.lstm.flatten_parameters()
             except RuntimeError:
                 pass
-            x = self.lstm(cFeature)[0]
+            x = self.lstm(x)[0]
+            if isinstance(x, torch.nn.utils.rnn.PackedSequence):
+                x = torch.nn.utils.rnn.pad_packed_sequence(x, batch_first=True)[0]
+        if self.useConvClassifier:
             x = x.permute(0, 2, 1)
             x = self.PhoneCriterionClassifier(x)
             return x.permute(0, 2, 1)
-            # return x
+        else:
+            return self.PhoneCriterionClassifier(x)
 
     def forward(self, cFeature, otherEncoded, label, computeAccuracy=False):
-        if isinstance(cFeature, dict):  # Second head uses smart pooling, therefore variable seqLens
+        onDownsampledHead = isinstance(cFeature, dict)
+        if onDownsampledHead:
             targetSizePred = cFeature['seqLens']
+            otherEncoded = cFeature['encodedData']
             cFeature = cFeature['states']
             B, _, H = cFeature.size()
+            if self.useLSTM:
+                cFeature = torch.nn.utils.rnn.pack_padded_sequence(cFeature, targetSizePred.cpu(), batch_first=True, enforce_sorted=False)
+                otherEncoded = torch.nn.utils.rnn.pack_padded_sequence(otherEncoded, targetSizePred.cpu(), batch_first=True, enforce_sorted=False)
         else:
             B, S, H = cFeature.size()
             targetSizePred = torch.ones(B, dtype=torch.int64,
                                     device=cFeature.device) * S
-        if not self.linear:
+        if self.useConvClassifier:
             targetSizePred = ((targetSizePred - 8) // 4 + 1)
 
-        if self.onEncoder:
-            predictions = self.getPrediction(otherEncoded)
-        else:
-            predictions = self.getPrediction(cFeature)
+        features = otherEncoded if self.onEncoder else cFeature
+        predictions = self.getPrediction(features)
         if self.forbid_blank:
             predictions += (
                 -1e4 * 
@@ -447,6 +473,18 @@ class CTCPhoneCriterion(BaseCriterion):
         loss = self.lossCriterion(predictions.permute(1, 0, 2), label,
                                   targetSizePred, sizeLabels).view(1, -1)
         avgPER = 0.
+        if computeAccuracy:
+            predictedPhones = predictions.max(2)[1].detach().cpu()
+            numSegments = torch.nonzero(torch.diff(predictedPhones[0, :]), as_tuple=True)[0]
+            # if random.random() < 0.005:
+            #     print("Predicted phones: ", predictedPhones[0, :int(numSegments[0].item())])
+            #     print("Expected phones: ", label[0, :sizeLabels[0]])
+            predictedPhones, sizePredictions = collapseLabelChain(predictedPhones)
+            for b in range(B):
+                predictedPhone = predictedPhones[b, :sizePredictions[b]]
+                predictedPhone = predictedPhone[predictedPhone != self.BLANK_LABEL]
+                avgPER += getSeqPER((predictedPhone, label[b, :sizeLabels[b]].cpu()))
+            avgPER /= B
         return loss, avgPER * torch.ones(1, 1, device=loss.device)
 
 

cpc/eval/common_voices_eval.py
@@ -17,7 +17,7 @@ import progressbar
 from pathlib import Path
 from torch.utils.data import Dataset, DataLoader
 from torch.multiprocessing import Pool
-from cpc.criterion.seq_alignment import get_seq_PER
+from cpc.criterion.seq_alignment import getSeqPER
 from cpc.criterion.seq_alignment import beam_search
 from cpc.feature_loader import loadModel
 from cpc.dataset import findAllSeqs, parseSeqLabels, filterSeqs
@@ -273,7 +273,7 @@ def get_per(data):
     p_ = pred[:l_].view(l_, -1).numpy()
     gt_seq = gt[:size_gt].view(-1).tolist()
     predSeq = beam_search(p_, 20, blank_label)[0][1]
-    out = get_seq_PER((gt_seq, predSeq))
+    out = getSeqPER((gt_seq, predSeq))
     return out
 
 

cpc/eval/linear_separability.py
@@ -20,9 +20,7 @@ import cpc.feature_loader as fl
 import cpc.utils.misc as utils
 from cpc.dataset import AudioBatchData, findAllSeqs, filterSeqs, parseSeqLabels
 from cpc.model import CPCModelNullspace
-from cpc.criterion.seq_alignment import collapseLabelChain, get_seq_PER
-from torch.multiprocessing import Pool
-# import pandas as pd
+# from cpc.criterion.seq_alignment import collapseLabelChain, getSeqPER
 
 
 def getCriterion(args, dim_features, dim_inter, speakers):
@@ -35,13 +33,18 @@ def getCriterion(args, dim_features, dim_inter, speakers):
         if not args.CTC:
             print(f"Running phone separability with aligned phones")
             criterion = cr.PhoneCriterion(dim_features,
-                                          nPhones, args.get_encoded)
+                                          nPhones, args.get_encoded,
+                                          useLSTM=args.useLSTM,
+                                          useConvClassifier=args.convClassifier,
+                                          linear=args.linearClassifier)
         else:
             print(f"Running phone separability with CTC loss")
             criterion = cr.CTCPhoneCriterion(dim_features,
                                              nPhones, 
                                              args.get_encoded,
-                                             nLayers=args.numLayers,
+                                             useLSTM=args.useLSTM,
+                                             useConvClassifier=args.convClassifier,
+                                             linear=args.linearClassifier,
                                              forbid_blank=args.CTC_forbid_blank)
     else:
         labelKey = 'speaker'
@@ -53,84 +56,6 @@ def getCriterion(args, dim_features, dim_inter, speakers):
     return criterion, labelKey, phoneLabels
 
 
-def perStep(valLoader,
-            model,
-            criterion,
-            CPCLevel,
-            pathCheckpoint):
-
-    model.eval()
-    criterion.eval()
-
-    avgPER = 0
-    varPER = 0
-    nItems = 0
-
-    # results = []
-
-    print("Starting the PER computation")
-    for step, fulldata in tqdm.tqdm(enumerate(valLoader)):
-        with torch.no_grad():
-            batch_data, label_data = fulldata
-            label = label_data['phone']
-            cFeature, encodedData, _ = model(batch_data, None)
-            cFeature = cFeature[CPCLevel]
-            if isinstance(cFeature, dict):  # Second head uses smart pooling, therefore variable seqLens
-                targetSizePred = cFeature['seqLens']
-                cFeature = cFeature['states']
-                B, _, H = cFeature.size()
-            else:
-                B, S, H = cFeature.size()
-                targetSizePred = torch.ones(B, dtype=torch.int64,
-                                        device=cFeature.device) * S
-            if not criterion.module.linear:
-                targetSizePred = ((targetSizePred - 8) // 4 + 1)
-            if criterion.module.onEncoder:
-                predictions = criterion.module.getPrediction(encodedData)
-            else:
-                predictions = criterion.module.getPrediction(cFeature)
-            if criterion.module.forbid_blank:
-                predictions += (
-                    -1e4 * 
-                    (
-                        torch.arange(criterion.module.BLANK_LABEL + 1, 
-                        device=predictions.device) == criterion.module.BLANK_LABEL
-                    ).float().view(1, 1, criterion.module.BLANK_LABEL + 1))
-            predictions = torch.nn.functional.log_softmax(predictions, dim=2)
-            label = label.to(predictions.device)
-            label, sizeLabels = collapseLabelChain(label)   
-            predictedPhones = predictions.max(2)[1].detach().cpu()
-            predictedPhones, sizePredictions = collapseLabelChain(predictedPhones)
-            # dataPER = []
-            poolData = []
-            for b in range(B):
-                predictedPhone = predictedPhones[b, :sizePredictions[b]]
-                predictedPhone = predictedPhone[predictedPhone != criterion.module.BLANK_LABEL]
-                poolData.append(get_seq_PER((predictedPhone, label[b, :sizeLabels[b]].cpu())))
-                # results.append({
-                #     'step': step,
-                #     'predicted': predictedPhone.tolist(),
-                #     'target': label[b, :sizeLabels[b]].tolist(),
-                #     'PER': poolData[-1]
-                # })
-                # dataPER.append((
-                #     predictedPhone, label[b, :sizeLabels[b]].cpu()
-                # ))
-            # with Pool(B) as p:
-            #     poolData = p.map(get_seq_PER, dataPER)
-            avgPER += sum([x for x in poolData])
-            varPER += sum([x*x for x in poolData])
-            nItems += len(poolData)
-    avgPER /= nItems
-    varPER /= nItems
-    varPER -= avgPER**2
-    print(f"Average PER {avgPER}")
-    print(f"Standard deviation PER {math.sqrt(varPER)}")
-    logs = {"avgPER": avgPER,  "stdPER": math.sqrt(varPER)}
-    # results = pd.DataFrame(results)
-    # results.to_csv('PER_results')
-    utils.save_logs(logs, f"{pathCheckpoint}_logsVal.json")
-
 
 def train_step(feature_maker, criterion, data_loader, optimizer, CPCLevel, label_key="speaker", centerpushSettings=None):
     if feature_maker.optimize:
@@ -138,15 +63,15 @@ def train_step(feature_maker, criterion, data_loader, optimizer, CPCLevel, label
     criterion.train()
     computeAccuracy = not isinstance(criterion.module, cr.CTCPhoneCriterion)
     if computeAccuracy:
-        logs = {"locLoss_train": 0,  "locAcc_train": 0}
+        logs = {"Loss_train": 0,  "Acc_train": 0}
     else:
-        logs = {"locLoss_train": 0}
+        logs = {"Loss_train": 0}
     for step, fulldata in tqdm.tqdm(enumerate(data_loader)):
 
         optimizer.zero_grad()
         batch_data, label_data = fulldata
         label = label_data[label_key]
-        c_feature, encoded_data, _ = feature_maker(batch_data, None)
+        c_feature, encoded_data, _ = feature_maker(batch_data, label)
         c_feature = c_feature[CPCLevel]
         if not feature_maker.optimize:
             encoded_data = encoded_data.detach()
@@ -165,9 +90,9 @@ def train_step(feature_maker, criterion, data_loader, optimizer, CPCLevel, label
         totLoss.backward()
         optimizer.step()
 
-        logs["locLoss_train"] += np.asarray([all_losses.mean().item()])
+        logs["Loss_train"] += np.asarray([all_losses.mean().item()])
         if computeAccuracy:
-            logs["locAcc_train"] += np.asarray([all_acc.mean().item()])
+            logs["Acc_train"] += np.asarray([all_acc.mean().item()])
 
     logs = utils.update_logs(logs, step)
     logs["iter"] = step
@@ -179,23 +104,24 @@ def val_step(feature_maker, criterion, data_loader, CPCLevel, computeAccuracy, l
     feature_maker.eval()
     criterion.eval()
     if computeAccuracy:
-        logs = {"locLoss_val": 0,  "locAcc_val": 0}
+        accLabel = "Acc_val" if not isinstance(criterion.module, cr.CTCPhoneCriterion) else "PER_val"
+        logs = {"Loss_val": 0,  accLabel: 0}
     else:
-        logs = {"locLoss_val": 0}
+        logs = {"Loss_val": 0}
     for step, fulldata in tqdm.tqdm(enumerate(data_loader)):
         with torch.no_grad():
             batch_data, label_data = fulldata
             label = label_data[label_key]
-            c_feature, encoded_data, _ = feature_maker(batch_data, None)
+            c_feature, encoded_data, _ = feature_maker(batch_data, label)
             c_feature = c_feature[CPCLevel]
             if centerpushSettings:
                 centers, pushDeg = centerpushSettings
                 c_feature = utils.pushToClosestForBatch(c_feature, centers, deg=pushDeg)
                 encoded_data = utils.pushToClosestForBatch(encoded_data, centers, deg=pushDeg)
             all_losses, all_acc = criterion(c_feature, encoded_data, label, computeAccuracy)
-            logs["locLoss_val"] += np.asarray([all_losses.mean().item()])
+            logs["Loss_val"] += np.asarray([all_losses.mean().item()])
             if computeAccuracy:
-                logs["locAcc_val"] += np.asarray([all_acc.mean().item()])
+                logs[accLabel] += np.asarray([all_acc.mean().item()])
     logs = utils.update_logs(logs, step)
     return logs
 
@@ -213,10 +139,11 @@ def run(feature_maker,
         centerpushSettings=None):
 
     start_epoch = len(logs["epoch"])
-    best_acc = -1
 
     start_time = time.time()
-
+    usesCTCLoss = isinstance(criterion.module, cr.CTCPhoneCriterion)
+    accLabel = "Acc_val" if not usesCTCLoss else "PER_val"
+    best_acc = -1 if not usesCTCLoss else np.inf
     for epoch in range(start_epoch, n_epochs):
 
         logs_train = train_step(feature_maker, criterion, train_loader,
@@ -236,9 +163,9 @@ def run(feature_maker,
         print('_'*50)
         print('')
 
-        if logs_val["locAcc_val"] > best_acc:
+        if (usesCTCLoss and logs_val[accLabel] < best_acc) or (not usesCTCLoss and logs_val[accLabel] > best_acc):
             best_state = deepcopy(fl.get_module(feature_maker).state_dict())
-            best_acc = logs_val["locAcc_val"]
+            best_acc = logs_val[accLabel]
 
         logs["epoch"].append(epoch)
         for key, value in dict(logs_train, **logs_val).items():
@@ -307,15 +234,15 @@ def trainLinsepClassification(
         print('_'*50)
         print('')
 
-        if logs_val["locAcc_val"] > best_acc:
+        if logs_val["Acc_val"] > best_acc:
             best_state_cpc = deepcopy(fl.get_module(feature_maker).state_dict())
             best_state_classif_crit = deepcopy(fl.get_module(criterion).state_dict())
             optimizer_state_best_ep = optimizer.state_dict()
             best_epoch = epoch
-            best_acc = logs_val["locAcc_val"]
+            best_acc = logs_val["Acc_val"]
 
-        if logs_train["locAcc_train"] > best_train_acc:
-            best_train_acc = logs_train["locAcc_train"]
+        if logs_train["Acc_train"] > best_train_acc:
+            best_train_acc = logs_train["Acc_train"]
 
         logs["epoch"].append(epoch)
         for key, value in dict(logs_train, **logs_val).items():
@@ -328,10 +255,6 @@ def trainLinsepClassification(
         if (epoch % logs["saveStep"] == 0 and epoch > 0) or epoch == n_epochs - 1:
             model_state_dict = fl.get_module(feature_maker).state_dict()
             criterion_state_dict = fl.get_module(criterion).state_dict()
-
-            # fl.save_checkpoint(model_state_dict, criterion_state_dict,
-            #                    optimizer.state_dict(), best_state,
-            #                    f"{path_checkpoint}_{epoch}.pt")
             utils.save_logs(logs, f"{path_logs}_logs.json")
 
     if path_best_checkpoint:
@@ -426,9 +349,14 @@ def parse_args(argv):
     parser.add_argument('--centerpushDeg', type=float, default=None, help="part of (euclidean) distance to push to the center")
 
     parser.add_argument('--CPCLevel', type=int, default=0, help="")
-    parser.add_argument('--numLayers', type=int, default=2, help="")
+    parser.add_argument('--linearClassifier', action='store_true')
+    parser.add_argument('--convClassifier', action='store_true')
+    parser.add_argument('--useLSTM', action='store_true')
 
     args = parser.parse_args(argv)
+    if args.CPCLevel > 0:
+        assert args.CTC, "To use features of a downsampled CPC head you must use CTC loss (ie. pass the --CTC flag)"
+        assert not args.convClassifier, "To use features of a downsampled CPC head you must use a frame-wise classifier (ie. do not pass the --convClassifier flag)"
     if args.nGPU < 0:
         args.nGPU = torch.cuda.device_count()
     if args.save_step <= 0:
@@ -587,7 +515,13 @@ def main(argv):
 
         with open(f"{args.pathCheckpoint}_args.json", 'w') as file:
             json.dump(vars(args), file, indent=2)
-        perStep(val_loader, model, criterion, args.CPCLevel, args.pathCheckpoint)
+        logs = val_step(model, criterion, val_loader, args.CPCLevel, computeAccuracy=True, 
+                        label_key=labelKey, centerpushSettings=centerpushSettings)
+        for key, value in dict(logs).items():
+            if isinstance(value, np.ndarray):
+                value = value.tolist()
+            logs[key] = value
+        utils.save_logs(logs, f"{pathCheckpoint}_logs.json")
     else:
         # Optimizer
         g_params = list(criterion.parameters())
@@ -633,12 +567,11 @@ def main(argv):
             label_key=labelKey, centerpushSettings=centerpushSettings)
 
 
-
 if __name__ == "__main__":
-    #import ptvsd
-    #ptvsd.enable_attach(('0.0.0.0', 7310))
-    #print("Attach debugger now")
-    #ptvsd.wait_for_attach()
+    # import ptvsd
+    # ptvsd.enable_attach(('0.0.0.0', 7310))
+    # print("Attach debugger now")
+    # ptvsd.wait_for_attach()
     
     torch.multiprocessing.set_start_method('spawn')
     args = sys.argv[1:]

cpc/model.py
@@ -173,7 +173,7 @@ class CPCAR(nn.Module):
                  minLengthSeqMinusOne,
                  mode="GRU",
                  reverse=False,
-                 segmentationType='jch',
+                 segmentationType='kreuk',
                  NoARonRegHead=False):
 
         super(CPCAR, self).__init__()
@@ -248,7 +248,8 @@ class CPCAR(nn.Module):
         for l in range(1, self.numLevels):
             if self.smartPooling:
                 minLengthSeq = max(1, int(round(2* self.minLengthSeqMinusOne * self.segmentationThreshold**l))) + 1
-                if label is not None:
+                if self.segmentationType == 'groundTruth':
+                    assert label is not None, "To use ground truth segmentation labels must be provided"
                     diffs = torch.diff(label, dim=1)
                     phoneChanges = torch.cat((torch.ones((label.shape[0], 1)).cuda(), diffs), dim=1)
                     boundaries = torch.nonzero(phoneChanges.contiguous().view(-1), as_tuple=True)[0]
@@ -382,17 +383,15 @@ class CPCModel(nn.Module):
 
     def __init__(self,
                  encoder,
-                 AR,
-                 label=None):
+                 AR):
 
         super(CPCModel, self).__init__()
         self.gEncoder = encoder
         self.gAR = AR
-        self.label = label
 
     def forward(self, batchData, label):
         encodedData = self.gEncoder(batchData).permute(0, 2, 1)
-        cFeature = self.gAR(encodedData) if self.label is None else self.gAR(encodedData, label)
+        cFeature = self.gAR(encodedData, label)
         return cFeature, encodedData, label
 
 class CPCModelNullspace(nn.Module):