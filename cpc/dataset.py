852b090fd5cebfb9c9b53a8bf008df10556dfad4
Anurag Kumar <anurag.kumar@rev.com>
2021-12-02 14:52:54 -0600

    speaker level set to 1

--- cpc/dataset.py
+++ cpc/dataset.py
@@ -436,6 +436,8 @@ class SameSpeakerSampler(Sampler):
 
 def extractLength(couple):
     speaker, locPath = couple
+    print(locPath, speaker)
+    unzip_file(locPath)
     #info = torchaudio.info(str(locPath))[0]
     info = torchaudio.info(str(locPath))
     return info.num_frames
